1a97747281284ad56a9046a7111eec40141f58e4
Sergey <manofthetreasure@gmail.com>
2022-09-30 12:32:59 +0300

    m

---
+++ script.js
@@ -0,0 +1,22 @@
+const detect = document.querySelector("#detect");
+const wrapper = document.querySelector(".wrapper");
+const button = document.querySelector("button");
+
+window.onload = () => {
+  let adClasses = ["ad", "ads", "adsbox", "ad-placement", "doubleclick", "ad-placeholder", "ad-badge"];
+
+  for (let item of adClasses) {
+    detect.classList.add(item);
+  }
+
+  let getProperty = window.getComputedStyle(detect).getPropertyValue("display");
+
+  if (!wrapper.classList.contains("show")) {
+    getProperty === "none" ? wrapper.classList.add("show") : wrapper.classList.remove("show");
+  }
+
+  button.addEventListener("click", () => {
+    wrapper.classList.remove("show");
+  })
+}
+
